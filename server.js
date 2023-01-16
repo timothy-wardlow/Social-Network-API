98b49b9ff9582c00970fd3eba2da4bb6cbd36c62
Timothy Wardlow <wardlowtimothy@icloud.com>
2023-01-16 15:32:35 -0600

    base build out, wip

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const db = require('./config/config.js');
+const routes = require('./routes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {app.listen(PORT, () => {console.log(`API server running on localhost:${PORT}`)})});
