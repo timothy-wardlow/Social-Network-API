4949cf24bfeb5d913c8ed1222a6d608abeb646e2
Timothy Wardlow <wardlowtimothy@icloud.com>
2023-01-16 15:36:50 -0600

    base build out, wip

--- server.js
+++ server.js
@@ -9,4 +9,4 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-db.once('open', () => {app.listen(PORT, () => {console.log(`API server running on localhost:${PORT}`)})});
+db.once('open', () => {app.listen(PORT, () => {console.log(`API server running on`)})})
